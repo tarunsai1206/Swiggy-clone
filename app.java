6a8b7b7bd0aef734761e971e90b949c47670fe4b
tarun <Tarunsai.Tulimilli@amazon.com>
2026-09-09 13:37:45 +0000

    developer-1 added code to app.java

---
+++ app.java
@@ -0,0 +1,15 @@
+```java
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
+```
+
